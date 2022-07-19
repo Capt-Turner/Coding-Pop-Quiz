cbc02c040d82dd580473518dce5d8cd65655cc6c
Seth Turner <sethhet@gmail.com>
2022-07-18 20:02:54 -0600

    initial infrastructure and psudo-code

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// when i press start, the game begins
+
+// when the game begins, a timer starts from 60 and the first question comes up
+
+// when i select an answer, logs if it is the correct or incorrect answer and moves to second question
+
+// if incorrect, incurs a small time penalty
+
+// repeats till end of quiz or timer = 0 where score is shown and initials are input and saved with score
+
+// maybe add a difficulty selector that determines length of quiz? 
